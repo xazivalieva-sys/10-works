e4400ac3133cd2b574aada97b924a07850cb6003
xazivalieva-sys <xazivalieva@bk.ru>
2025-10-04 17:01:47 +0300

    Решена только задача 1 и 2

--- 2 работа.py
+++ 2 работа.py
@@ -5,13 +5,7 @@ print("Введите элементы матрицы построчно:")
 for i in range(M):
     row = list(map(int, input(f"Строка {i + 1}: ").split()))
     B.append(row)
-min_array = []
-for j in range(N):
-    min_val = B[0][j]
-    for i in range(1, M):
-        if B[i][j] < min_val:
-            min_val = B[i][j]
-    min_array.append(min_val)
+min_array = [min(B[i][j] for i in range(M)) for j in range(N)]
 print("\nИсходная матрица:")
 for row in B:
     print(row)
